850007e9961834c177e81ddf1ec97b7ed232f217
VICTORIA19981997 <51470597+VICTORIA19981997@users.noreply.github.com>
2019-06-13 07:18:23 -0500

    programacion administradores

--- kinder/ADMINISTRADORES.cs
+++ kinder/ADMINISTRADORES.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,11 +14,12 @@ namespace Kinder
 {
     public partial class ADMINISTRADORES : Form
     {
+        admin cmda = new admin();
         public ADMINISTRADORES()
         {
             InitializeComponent();
             Bitmap img = new Bitmap(Application.StartupPath + @"\img\fondo.jpg");
-            this.Background= img;
+            this.BackgroundImage = img;
             this.BackgroundImageLayout = ImageLayout.Stretch;
         }
 
@@ -27,7 +30,7 @@ namespace Kinder
 
         private void ADMINISTRADORES_Load(object sender, EventArgs e)
         {
-
+            cmda.llenargrid(dataGridViewADMIN);
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -39,5 +42,20 @@ namespace Kinder
             //OCULTAMOS 
             Visible = false;
         }
+        class admin
+        {
+            MySqlConnection con = new MySqlConnection("server = 127.0.0.1; database=proyecto;Uid=root");
+
+            public void llenargrid(DataGridView grid)
+            {
+                MySqlCommand cn = new MySqlCommand("select * from proyecto.usuarios;", con);
+                MySqlDataAdapter da = new MySqlDataAdapter(cn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                grid.DataSource = dt;
+            }
+        }
     }
+
 }
+
