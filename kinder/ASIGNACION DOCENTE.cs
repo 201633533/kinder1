66c216304674e635a78bb95470e1225c05a8ef0a
VICTORIA19981997 <51470597+VICTORIA19981997@users.noreply.github.com>
2019-06-13 07:51:26 -0500

    PROGRAMACION EN ASIGNACION ALUMNOS

--- kinder/ASIGNACION DOCENTE.cs
+++ kinder/ASIGNACION DOCENTE.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,7 @@ namespace Kinder
 {
     public partial class ASIGNACION_DOCENTE : Form
     {
+        asigdoc cmda = new asigdoc();
         public ASIGNACION_DOCENTE()
         {
             InitializeComponent();
@@ -22,7 +24,7 @@ namespace Kinder
 
         private void ASIGNACION_DOCENTE_Load(object sender, EventArgs e)
         {
-
+            cmda.llenargrid(dataGridViewasigdocente);
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -33,6 +35,19 @@ namespace Kinder
             obj.Visible = true;
             //OCULTAMOS 
             Visible = false;
+        }
+        class asigdoc
+        {
+            MySqlConnection con = new MySqlConnection("server = 127.0.0.1; database=proyecto;Uid=root");
+
+            public void llenargrid(DataGridView grid)
+            {
+                MySqlCommand cn = new MySqlCommand("select * from proyecto.asigGradoyGrupo;", con);
+                MySqlDataAdapter da = new MySqlDataAdapter(cn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                grid.DataSource = dt;
+            }
         }
     }
 }
