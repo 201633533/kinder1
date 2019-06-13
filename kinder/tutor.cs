1f64c24ce804b6d048cb7381bc1981e615b5d7f5
VICTORIA19981997 <51470597+VICTORIA19981997@users.noreply.github.com>
2019-06-13 08:02:57 -0500

    programacion en tutor

---
+++ kinder/tutor.cs
@@ -0,0 +1,56 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kinder
+{
+    public partial class tutor : Form
+    {
+        tutores cmda = new tutores();
+        public tutor()
+        {
+            InitializeComponent();
+        }
+
+        private void tutor_Load(object sender, EventArgs e)
+        {
+            cmda.llenargrid(dataGridView1);
+        }
+        class tutores
+        {
+            MySqlConnection con = new MySqlConnection("server = 127.0.0.1; database=proyecto;Uid=root");
+
+            public void llenargrid(DataGridView grid)
+            {
+                MySqlCommand cn = new MySqlCommand("select * from proyecto.usuarios;", con);
+                MySqlDataAdapter da = new MySqlDataAdapter(cn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                grid.DataSource = dt;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //CREAMOS LA INSTANCIA DEL FORMULARIO
+            INICIO_SESION_ADMINISTRADOR obj = new INICIO_SESION_ADMINISTRADOR();
+            //LLAMAMOS EL OBJETO Y LO PONEMOS VISIBLE
+            obj.Visible = true;
+            //OCULTAMOS 
+            Visible = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
